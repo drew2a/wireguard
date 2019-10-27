4a421a90809ab150ab7c232131dd9ea38de60924
drew2a <for.drew@gmail.com>
2019-10-27 21:11:17 +0300

    add wg-genconf.sh

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# wireguard
-Simplifying routine operations with wireguard
+# Wireguard
+
+Simplifying routine operations with [WireGuard](https://www.wireguard.com)
+
+# Prerequisites
+
+Install [WireGuard](https://www.wireguard.com) if it's not installed.
+
+## wg-genconf.sh
+
+This script generate server and clients configs for WireGuard.
+
+If the public IP is not defined, then the public IP of the machine from which the script is run is used.
+
+If the number of clients is not defined, then used 5 clients.
+
+### Usage
+
+```bash
+./wg-genconf.sh [<server_public_ip> <number_of_clients>]
+```
+
+### Example of usage:
+
+```bash
+./wg-genconf.sh 157.245.73.253 10
+```
---
+++ wg-genconf.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+#wg-genconf.sh ip 15
+
+server_ip=${1:-$(dig @resolver1.opendns.com ANY myip.opendns.com +short)}
+clients_count=${2:-5}
+
+server_private_key=$(wg genkey)
+server_public_key=$(echo "${server_private_key}" | wg pubkey)
+server_config=wg0.conf
+
+echo Generate server \("${server_ip}"\) config:
+echo
+echo -e "\t$(pwd)/${server_config}"
+#
+# server configs
+#
+cat > "${server_config}" <<EOL
+[Interface]
+Address = 10.0.0.1/24
+SaveConfig = true
+ListenPort = 51820
+PrivateKey = ${server_private_key}
+
+PostUp = iptables -A FORWARD -i %i -j ACCEPT; iptables -A FORWARD -o %i -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+PostDown = iptables -D FORWARD -i %i -j ACCEPT; iptables -D FORWARD -o %i -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE
+EOL
+
+echo
+echo Generate configs for "${clients_count}" clients:
+echo
+#
+# clients configs
+#
+for i in $(seq 1 "${clients_count}");
+do
+
+    client_private_key=$(wg genkey)
+    client_public_key=$(echo "${client_private_key}" | wg pubkey)
+    client_ip=10.0.0.$((i+1))/32
+    client_config=client$i.conf
+    echo -e "\t$(pwd)/${client_config}"
+  	cat > "${client_config}" <<EOL
+[Interface]
+PrivateKey = ${client_private_key}
+ListenPort = 51820
+Address = ${client_ip}
+DNS = 8.8.8.8
+
+[Peer]
+PublicKey = ${server_public_key}
+AllowedIPs = 0.0.0.0/0
+Endpoint = ${server_ip}:51820
+PersistentKeepalive = 21
+EOL
+
+    cat >> "${server_config}" <<EOL
+[Peer]
+PublicKey = ${client_public_key}
+AllowedIPs = ${client_ip}
+EOL
+done
+
